59e81699dc558b46d75fa0c9e24da6d2607b5d0b
lucia <lucia.chen189@gmail.com>
2023-07-20 16:47:31 +0200

    blog 40

---
+++ ompl/MyOptimalPlanning.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python
+
+######################################################################
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2010, Rice University
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#   * Neither the name of the Rice University nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+######################################################################
+
+# Author: Luis G. Torres, Mark Moll
+
+import sys
+import cv2
+import numpy as np
+try:
+    from ompl import util as ou
+    from ompl import base as ob
+    from ompl import geometric as og
+except ImportError:
+    # if the ompl module is not in the PYTHONPATH assume it is installed in a
+    # subdirectory of the parent directory called "py-bindings."
+    from os.path import abspath, dirname, join
+    sys.path.insert(0, join(dirname(dirname(abspath(__file__))), 'py-bindings'))
+    from ompl import util as ou
+    from ompl import base as ob
+    from ompl import geometric as og
+from math import sqrt
+import argparse
+
+
+wi = 10
+hi = 10
+wf = 211
+hf = 233
+
+folder = "results/"
+group = "C_"
+img_extension = ".png"
+
+maxWidth_ = 0
+maxHeight_ = 0
+
+map_im = cv2.imread('png/map1.png')
+height, width, _ = map_im.shape
+maxWidth_ = width - 1
+maxHeight_ = height - 1
+
+# Our "collision checker". 
+class ValidityChecker(ob.StateValidityChecker):
+
+    def isValid(self, state):
+        w = min(int(state[0]), maxWidth_)
+        h = min(int(state[1]), maxHeight_)
+
+        c = map_im[h, w]
+        return c[0] > 250 and c[1] > 250 and c[2] > 250
+
+
+## Returns a structure representing the optimization objective to use
+#  for optimal motion planning. This method returns an objective
+#  which attempts to minimize the length in configuration space of
+#  computed paths.
+def getPathLengthObjective(si):
+    return ob.PathLengthOptimizationObjective(si)
+
+## Returns an optimization objective which attempts to minimize path
+#  length that is satisfied when a path of length shorter than 1.51
+#  is found.
+def getThresholdPathLengthObj(si):
+    obj = ob.PathLengthOptimizationObjective(si)
+    obj.setCostThreshold(ob.Cost(330))
+    return obj
+
+## Create an optimization objective for minimizing path length, and
+#  specify a cost-to-go heuristic suitable for this optimal planning
+#  problem.
+def getPathLengthObjWithCostToGo(si):
+    obj = ob.PathLengthOptimizationObjective(si)
+    obj.setCostToGoHeuristic(ob.CostToGoHeuristic(ob.goalRegionCostToGo))
+    return obj
+
+
+# Keep these in alphabetical order and all lower case
+def allocatePlanner(si, plannerType):
+    if plannerType.lower() == "bfmtstar":
+        return og.BFMT(si)
+    elif plannerType.lower() == "bitstar":
+        return og.BITstar(si)
+    elif plannerType.lower() == "fmtstar":
+        return og.FMT(si)
+    elif plannerType.lower() == "informedrrtstar":
+        return og.InformedRRTstar(si)
+    elif plannerType.lower() == "prmstar":
+        return og.PRMstar(si)
+    elif plannerType.lower() == "rrtstar":
+        return og.RRTstar(si)
+    elif plannerType.lower() == "sorrtstar":
+        return og.SORRTstar(si)
+    else:
+        ou.OMPL_ERROR("Planner-type is not implemented in allocation function.")
+
+
+# Keep these in alphabetical order and all lower case
+def allocateObjective(si, objectiveType):
+    if objectiveType.lower() == "pathlength":
+        return getPathLengthObjective(si)
+    elif objectiveType.lower() == "thresholdpathlength":
+        return getThresholdPathLengthObj(si)
+    else:
+        ou.OMPL_ERROR("Optimization-objective is not implemented in allocation function.")
+
+# 
+def erode_img():
+    kernel = np.ones((8, 8), np.uint8)
+    return cv2.erode(map_im, kernel)
+
+# Paint solution path on the map
+def recordSolution(pdef, map_img):
+    p = pdef.getSolutionPath()  
+    p.interpolate()     # Crear trayectoria
+    save(draw(p, map_img), folder + group + "result" + img_extension)
+
+# Paint the solution path on the map
+def draw(p, map_img):
+    for i in range(p.getStateCount()):
+        w = min(maxWidth_, int(p.getState(i)[0]))
+        h = min(maxHeight_, int(p.getState(i)[1]))
+        map_img[h, w] = (255, 0, 0)
+    for i in range(3):
+        for j in range(3):
+            map_img[i+hi-1,j+wi-1] = (0, 255, 0)
+            map_img[i+hf-1,j+wf-1] = (0, 0, 255)
+    
+    return map_img
+
+# Save the solution path in a file
+def save(map_img, filename):
+    cv2.imwrite(filename, map_img)
+
+def plan(runTime, plannerType, objectiveType, fname):
+    global map_im
+
+    space = ob.RealVectorStateSpace()
+    space.addDimension(0.0, width)
+    space.addDimension(0.0, height)
+
+    map_im = erode_img()
+
+    # Construct a space information instance for this state space
+    si = ob.SpaceInformation(space)
+
+    # Set the object used to check which states in the space are valid
+    validityChecker = ValidityChecker(si)
+    si.setStateValidityChecker(validityChecker)
+
+    si.setup()
+
+    # Set our robot's starting state 
+    start = ob.State(space)
+    start[0] = wi
+    start[1] = hi
+
+    # Set our robot's goal state 
+    goal = ob.State(space)
+    goal[0] = wf
+    goal[1] = hf
+
+    # Create a problem instance
+    pdef = ob.ProblemDefinition(si)
+
+    # Set the start and goal states
+    pdef.setStartAndGoalStates(start, goal)
+
+    # Create the optimization objective specified by our command-line argument.
+    # This helper function is simply a switch statement.
+    pdef.setOptimizationObjective(allocateObjective(si, objectiveType))
+
+    # Construct the optimal planner specified by our command line argument.
+    # This helper function is simply a switch statement.
+    optimizingPlanner = allocatePlanner(si, plannerType)
+
+    # Set the problem instance for our planner to solve
+    optimizingPlanner.setProblemDefinition(pdef)
+    optimizingPlanner.setup()
+
+    # attempt to solve the planning problem in the given runtime
+    solved = optimizingPlanner.solve(runTime)
+
+    if solved:
+        # Output the length of the path found
+        print('{0} found solution of path length {1:.4f} with an optimization ' \
+            'objective value of {2:.4f}'.format( \
+            optimizingPlanner.getName(), \
+            pdef.getSolutionPath().length(), \
+            pdef.getSolutionPath().cost(pdef.getOptimizationObjective()).value()))
+    
+        print(pdef.getSolutionPath())
+        recordSolution(pdef, map_im)
+        
+        #print(pdef.getSolutionPath().printAsMatrix())
+    else:
+        print("No solution found.")
+
+if __name__ == "__main__":
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='Optimal motion planning demo program.')
+
+    # Add a filename argument
+    parser.add_argument('-t', '--runtime', type=float, default=1.0, help=\
+        '(Optional) Specify the runtime in seconds. Defaults to 1 and must be greater than 0.')
+    parser.add_argument('-p', '--planner', default='RRTstar', \
+        choices=['BFMTstar', 'BITstar', 'FMTstar', 'InformedRRTstar', 'PRMstar', 'RRTstar', \
+        'SORRTstar', 'RRT'], \
+        help='(Optional) Specify the optimal planner to use, defaults to RRTstar if not given.')
+    parser.add_argument('-o', '--objective', default='PathLength', \
+        choices=['PathLength', 'ThresholdPathLength', \
+        'WeightedLengthAndClearanceCombo'], \
+        help='(Optional) Specify the optimization objective, defaults to PathLength if not given.')
+    parser.add_argument('-f', '--file', default=None, \
+        help='(Optional) Specify an output path for the found solution path.')
+    parser.add_argument('-i', '--info', type=int, default=0, choices=[0, 1, 2], \
+        help='(Optional) Set the OMPL log level. 0 for WARN, 1 for INFO, 2 for DEBUG.' \
+        ' Defaults to WARN.')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Check that time is positive
+    if args.runtime <= 0:
+        raise argparse.ArgumentTypeError(
+            "argument -t/--runtime: invalid choice: %r (choose a positive number greater than 0)" \
+            % (args.runtime,))
+
+    # Set the log level
+    if args.info == 0:
+        ou.setLogLevel(ou.LOG_WARN)
+    elif args.info == 1:
+        ou.setLogLevel(ou.LOG_INFO)
+    elif args.info == 2:
+        ou.setLogLevel(ou.LOG_DEBUG)
+    else:
+        ou.OMPL_ERROR("Invalid log-level integer.")
+
+    # Solve the planning problem
+    plan(args.runtime, args.planner, args.objective, args.file)
+
+## @endcond
